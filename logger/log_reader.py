942601921e401f22dd7003f39d95f0bb2ccf47c4
dualspectre <education.dual@gmail.com>
2025-08-27 22:42:48 +0300

    Добавлены функции для чтения и обработки логов в log_reader.py и main.py

---
+++ logger/log_reader.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import re
+
+
+def parse_log_line(line: str)->dict:
+    pass
+
+def load_logs(file_path: str) ->list:
+    pass
+
+def filter_logs_by_level(logs: list, level: str) -> list:
+    pass
+
+def count_logs_by_level(logs: list)->dict:
+    pass
---
+++ logger/main.py
@@ -0,0 +1,9 @@
+import log_reader
+from sys import argv
+
+def main():
+    
+    log_file = argv[1]
+
+if __name__ == "__main__":
+    main()
