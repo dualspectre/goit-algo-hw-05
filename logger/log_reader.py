339dc638cbb17511413c291ffcfb87a01310edc5
dualspectre <education.dual@gmail.com>
2025-09-15 23:00:39 +0300

    Добавлены функции для разбора и загрузки логов в log_reader.py; добавлен файл с логами logs.txt

--- logger/log_reader.py
+++ logger/log_reader.py
@@ -3,13 +3,27 @@ import re
 
 
 def parse_log_line(line: str)->dict:
-    pass
+    log_data = {}
+    log_pattern = r"^(?P<date>\d{4}-\d{2}-\d{2})\s(?P<time>\d{2}:\d{2}:\d{2})\s(?P<level>INFO|DEBUG|ERROR|WARNING)\s(?P<message>.*)$"
+    match = re.match(log_pattern, line)
+    if match:
+        log_data = match.groupdict()
+    return log_data
 
 def load_logs(file_path: str) ->list:
-    pass
+    logs = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            log_data = parse_log_line(line.strip())
+            if log_data:
+                logs.append(log_data)
+    return logs
 
 def filter_logs_by_level(logs: list, level: str) -> list:
     pass
 
 def count_logs_by_level(logs: list)->dict:
+    pass
+
+def display_log_counts(counts: dict):
     pass
--- logger/main.py
+++ logger/main.py
@@ -1,9 +1,14 @@
 import log_reader
 from sys import argv
+import re
 
 def main():
     
-    log_file = argv[1]
+    # log_file = argv[1]
+    with open('logs.txt', 'r') as file:
+        lines = [el.strip() for el in file.readlines()]
+    
+
 
 if __name__ == "__main__":
     main()
