9c71b88c5c92d79ba392964a7523217c0ef1387a
dualspectre <education.dual@gmail.com>
2025-09-16 23:51:03 +0300

    Обновлен .gitignore для правильного исключения файлов; добавлены функции загрузки, фильтрации и подсчета логов в log_reader.py; обновлен main.py для использования новых функций.

--- logger/log_reader.py
+++ logger/log_reader.py
@@ -1,7 +1,6 @@
-from datetime import datetime
 import re
 
-
+# Function to parse a log line
 def parse_log_line(line: str)->dict:
     log_data = {}
     log_pattern = r"^(?P<date>\d{4}-\d{2}-\d{2})\s(?P<time>\d{2}:\d{2}:\d{2})\s(?P<level>INFO|DEBUG|ERROR|WARNING)\s(?P<message>.*)$"
@@ -10,7 +9,14 @@ def parse_log_line(line: str)->dict:
         log_data = match.groupdict()
     return log_data
 
+# Function to load logs from file
 def load_logs(file_path: str) ->list:
+    """
+    Load logs from a specified file and return a list of log entries.
+    Input: file_path (str) - Path to the log file.
+    
+    Return: List of log entries (list of dicts).
+    """
     logs = []
     with open(file_path, 'r') as file:
         for line in file:
@@ -20,10 +26,19 @@ def load_logs(file_path: str) ->list:
     return logs
 
 def filter_logs_by_level(logs: list, level: str) -> list:
-    pass
+    filtered_logs = filter(lambda log: log.get('level') == level, logs)
+    return filtered_logs
 
 def count_logs_by_level(logs: list)->dict:
-    pass
+    logs_counts = {}
+    for log in logs:
+        level = log.get('level')
+        if level:
+            logs_counts[level] = logs_counts.get(level, 0) + 1
+    return logs_counts
 
 def display_log_counts(counts: dict):
-    pass
+    print(f"{'Log level':<10} | {'Counts':<6}")
+    print("-" * 11,"|","-" * 7, sep="")
+    for level, count in counts.items():
+        print(f"{level:<10} | {count:<6}")
--- logger/main.py
+++ logger/main.py
@@ -1,13 +1,22 @@
 import log_reader
 from sys import argv
-import re
+
 
 def main():
     
     # log_file = argv[1]
-    with open('logs.txt', 'r') as file:
-        lines = [el.strip() for el in file.readlines()]
-    
+    try:
+        log_file = 'logs.txt'
+        logs = log_reader.load_logs(log_file)
+        log_counts = log_reader.count_logs_by_level(logs)
+        # print(logs)
+        # print(log_counts)
+        # fil_logs = log_reader.filter_logs_by_level(logs, 'ERROR')
+        # print(list(fil_logs))
+    except Exception as e:
+        print(f"Error: {e}")
+        return
+    log_reader.display_log_counts(log_counts)
 
 
 if __name__ == "__main__":
