0fd6b73f883fcddb04e3c9d632c622261c67d6ee
dualspectre <education.dual@gmail.com>
2025-09-17 14:23:43 +0300

    Добавлены функции для обработки логов: парсинг, фильтрация и подсчет по уровням; обновлен main.py для использования новых функций.

--- logger/log_reader.py
+++ logger/log_reader.py
@@ -2,6 +2,11 @@ import re
 
 # Function to parse a log line
 def parse_log_line(line: str)->dict:
+    """
+    Parse a single log line and return a dictionary with log details.
+    Input: line (str) - A single line from the log file.
+    Return: Dictionary with log details (dict).
+    """
     log_data = {}
     log_pattern = r"^(?P<date>\d{4}-\d{2}-\d{2})\s(?P<time>\d{2}:\d{2}:\d{2})\s(?P<level>INFO|DEBUG|ERROR|WARNING)\s(?P<message>.*)$"
     match = re.match(log_pattern, line)
@@ -14,7 +19,6 @@ def load_logs(file_path: str) ->list:
     """
     Load logs from a specified file and return a list of log entries.
     Input: file_path (str) - Path to the log file.
-    
     Return: List of log entries (list of dicts).
     """
     logs = []
@@ -25,11 +29,24 @@ def load_logs(file_path: str) ->list:
                 logs.append(log_data)
     return logs
 
+# Function to filter logs by level
 def filter_logs_by_level(logs: list, level: str) -> list:
+    """
+    Filter logs by a specified log level.
+    Input: logs (list) - List of log entries (list of dicts).
+           level (str) - Log level to filter by (e.g., 'INFO', 'ERROR').
+    Return: Filtered list of log entries (list of dicts).
+    """
     filtered_logs = filter(lambda log: log.get('level') == level, logs)
-    return filtered_logs
+    return list(filtered_logs)
 
+# Function to count logs by level
 def count_logs_by_level(logs: list)->dict:
+    """
+    Count the number of logs for each log level.
+    Input: logs (list) - List of log entries (list of dicts).
+    Return: Dictionary with log levels as keys and their counts as values (dict).
+    """
     logs_counts = {}
     for log in logs:
         level = log.get('level')
@@ -37,7 +54,13 @@ def count_logs_by_level(logs: list)->dict:
             logs_counts[level] = logs_counts.get(level, 0) + 1
     return logs_counts
 
+# Function to display log counts
 def display_log_counts(counts: dict):
+    """
+    Display the counts of logs by level in a formatted manner.
+    Input: counts (dict) - Dictionary with log levels as keys and their counts as values (dict).
+    Return: None
+    """
     print(f"{'Log level':<10} | {'Counts':<6}")
     print("-" * 11,"|","-" * 7, sep="")
     for level, count in counts.items():
--- logger/main.py
+++ logger/main.py
@@ -4,19 +4,34 @@ from sys import argv
 
 def main():
     
-    # log_file = argv[1]
-    try:
-        log_file = 'logs.txt'
-        logs = log_reader.load_logs(log_file)
-        log_counts = log_reader.count_logs_by_level(logs)
-        # print(logs)
-        # print(log_counts)
-        # fil_logs = log_reader.filter_logs_by_level(logs, 'ERROR')
-        # print(list(fil_logs))
-    except Exception as e:
-        print(f"Error: {e}")
+    args_counts = len(argv)
+    if args_counts != 1:
+        # Input file name logs
+        log_file = argv[1]
+
+        try:
+            logs = log_reader.load_logs(log_file)
+            log_counts = log_reader.count_logs_by_level(logs)
+            log_reader.display_log_counts(log_counts)
+        except FileNotFoundError:
+            print(f"Error: File '{log_file}' not found.")
+            return
+        except Exception as e:
+            print(f"Error: {e}")
+            return
+    else:
+        print("Usage: python main.py <log_file_path> [log_level]")
         return
-    log_reader.display_log_counts(log_counts)
+    
+    if args_counts > 2:
+        log_level = argv[2].upper()
+        filtered_logs = log_reader.filter_logs_by_level(logs, log_level)
+        if filtered_logs:
+            print(f"\nDetails logs with level '{log_level}':")
+            for log in filtered_logs:
+                print(f"{log['date']} {log['time']} - {log['message']}")
+        else:
+            print(f"\nNo logs found with level '{log_level}'.")
 
 
 if __name__ == "__main__":
